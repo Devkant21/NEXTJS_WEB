540de49512409e8c5d853de37037f118abfc7710
Devkant <devkant21@gmail.com>
2022-02-12 11:38:21 +0530

    Navbar

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+const about = () => {
+  return (
+    <div>
+        <Head>
+            <title>
+                About
+            </title>
+        </Head>
+        <h1>about</h1>
+    </div>
+  )
+}
+
+export default about
