af4247093e7e1f514149e5cab19e6261353ea897
Devkant <devkant21@gmail.com>
2022-02-12 12:11:50 +0530

    Navbar working fine. Issue fixed

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,14 @@
-import Nav from "./Nav"
-import styles from "../styles/Layout.module.css"
+import Nav from './Nav'
+import styles from '../styles/Layout.module.css'
 
 const Layout = ({ children }) => {
   return (
       <>
         <Nav />
         <div className={styles.container}>
-            <main className={styles.main}>{ children }
+            <main className={styles.main}>
+              <h1>Hello</h1>
+              {children}
             </main>
         </div> 
       </>  
